5d60ee21cf8b027e3461c2e97fceab79f644608f
Xu Han <51745690+XuHan-CN@users.noreply.github.com>
2020-03-25 21:38:27 +0800

    Create I.java

---
+++ src/eLuoSiFangKuai/I.java
@@ -0,0 +1,14 @@
+package eLuoSiFangKuai;
+
+public class I extends Tetromino{
+	/*
+	 * 提供构造器进行初始化
+	 * I型的四格方块的位置
+	*/
+	public I() {
+		cells[0]=new Cell(0,4,Tetris.I);
+		cells[1]=new Cell(0,3,Tetris.I);
+		cells[2]=new Cell(0,5,Tetris.I);
+		cells[3]=new Cell(0,6,Tetris.I);
+	}
+}
